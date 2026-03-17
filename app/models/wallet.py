3115e4601cd4956cd21422b95bfd0f73681980fd
muchumi <muchumimaina94@gmail.com>
2026-03-17 05:48:23 +0300

    created wallet model

---
+++ app/models/wallet.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
+from app.db.database import Base
+
+class Wallet(Base):
+    __tablename__ = "wallets"
+
+    id = Column(Integer, primary_key=True, index=True)
+    balance = Column(Float, default=0)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="wallet")
