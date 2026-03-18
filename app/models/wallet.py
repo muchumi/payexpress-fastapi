6266f2db5a34c1f4919d000e5cf0f41e488662c3
muchumi <muchumimaina94@gmail.com>
2026-03-18 05:12:32 +0300

    updates

--- app/models/wallet.py
+++ app/models/wallet.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from app.db.database import Base
+from app.models.user import User
 
 class Wallet(Base):
     __tablename__ = "wallets"
