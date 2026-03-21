4b42ab7372779962561bb9fc07079cd72b10056d
muchumi <muchumimaina94@gmail.com>
2026-03-21 05:56:46 +0300

    updated amountRequest pydantic model

--- app/schemas/amountRequest.py
+++ app/schemas/amountRequest.py
@@ -5,3 +5,4 @@ class AmountRequest(BaseModel):
     amount: float = Field(..., gt=0, description="The amount to be processed must be greater than zero")
     currency: str = "KES" #Default currency set to kenyan shillings
     description: Optional[str] = None
+    
