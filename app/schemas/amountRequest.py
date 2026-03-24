dc4950da16fb5a98384a7a8ecc61f5d4f454e6b2
muchumi <muchumimaina94@gmail.com>
2026-03-24 05:45:49 +0300

    updated the AmountRequest pydantic model default currency

--- app/schemas/amountRequest.py
+++ app/schemas/amountRequest.py
@@ -3,6 +3,6 @@ from typing import Optional
 
 class AmountRequest(BaseModel):
     amount: float = Field(..., gt=0, description="The amount to be processed must be greater than zero")
-    currency: str = "KES" #Default currency set to kenyan shillings
+    currency: str = Field(default="KES", description="The default currency of the amount set to KES") 
     description: Optional[str] = None
     
