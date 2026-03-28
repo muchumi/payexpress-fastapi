56f8a428bb6ce84eeb9121ab4abc700cc48d9e73
muchumi <muchumimaina94@gmail.com>
2026-03-28 18:17:26 +0300

    updating the withdraw route

--- app/main.py
+++ app/main.py
@@ -127,9 +127,14 @@ def withdraw(request: AmountRequest, current_user: User = Depends(get_current_us
     if not wallet:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="wallet resource not found")
     current_balance = wallet.balance or 0
+
     # preventing overdraft
+    if request.amount <= 0:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Withdrawal amount must be greater than zero")
+    
     if request.amount > current_balance:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Insufficient funds")
+    
     try:
         # deducting balance
         wallet.balance = current_balance - request.amount
