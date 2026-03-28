da9dfe1f5bd8077523c70c459cecf9bb355da0b9
muchumi <muchumimaina94@gmail.com>
2026-03-28 18:03:48 +0300

    adding comments in dependencies file

--- app/dependencies.py
+++ app/dependencies.py
@@ -15,13 +15,19 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: session = Depends(
         detail="could not validate credentials"
     )
     try:
+        # decode JWT token to get the email of the user
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         email=payload.get("sub")
+        # validating if the email exists
         if email is None:
             raise credentials_exception
     except JWTError:
         raise credentials_exception
+    # querying the database to get the user with the email from the token
     user = db.query(User).filter(User.email==email).first()
     if user is None:
         raise credentials_exception
     return user  
+
+
+
