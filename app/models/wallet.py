9f324fa15cef5c2f097c00b29149daefc31e7876
muchumi <muchumimaina94@gmail.com>
2026-04-19 05:20:09 +0300

    updates

--- app/models/wallet.py
+++ app/models/wallet.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, DateTime, Integer, Numeric, ForeignKey, func
+from sqlalchemy import Column, DateTime, Integer, Numeric, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 from app.db.database import Base
