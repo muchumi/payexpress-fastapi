4a37d1d63a5556c27eaaa7e5bce8b41c499b4606
muchumi <muchumimaina94@gmail.com>
2026-05-10 05:27:12 +0300

    replaced datetime.utcnow() which is deprecated in newer python versions since it creates a timezone-naive datetime

--- app/auth.py
+++ app/auth.py
@@ -1,5 +1,5 @@
 from jose import JWTError, jwt
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, UTC
 
 SECRET_KEY = "your_secret_key"
 ALGORITHM = "HS256"
@@ -8,7 +8,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 def create_access_token(data: dict):
     to_encode = data.copy()
 
-    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.now(UTC) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
 
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
