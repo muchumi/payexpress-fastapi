f8d68f44d03deaf26bae4122467dca21dbaa0b2f
muchumi <muchumimaina94@gmail.com>
2026-05-17 15:42:55 +0300

    updated class Config PV1 to ConfigDict PV2

--- app/schemas/paginatedTransactionResponse.py
+++ app/schemas/paginatedTransactionResponse.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from typing import List
 from .transactionHistoryResponse import TransactionHistoryResponse
 
@@ -8,7 +8,6 @@ class PaginatedTransactionResponse(BaseModel):
     offset: int
     data: List[TransactionHistoryResponse]
     
-    class Config:
-        from_attributes = True 
+    model_config=ConfigDict(from_attributes = True) 
         
     
